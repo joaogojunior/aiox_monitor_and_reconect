07bbddfffb2fc925985b16bb8b5e6822c0bc4920
João Guilherme <joaogojunior@gmail.com>
2022-08-06 10:39:39 -0300

    Adicionado mais comentarios e limpeza do codigo, removido dist do git e atualizado readme.md.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # aiox_monitor_and_reconnect
-This is a quick hack to get the buggy router a-wr307n to connect as a client on a wifi network and bridge with its ethernet ports.
-Also this script will monitor for wifi connectivity and when disconnected it tries to restablish the connection and restarts the router.
-The windows executable is 64bit and was created with py2exe.
-Parameters are stored in config.json file.
+This is a quick hack to get the buggy router a-wr307n to connect as a client on a wifi network and bridge with its
+ethernet ports.
+Also this script will monitor for wifi connectivity and when disconnected it tries to restablish the connection and
+restarts the router.
+The motivation comes from the fact that some access points periodically changes channel over time and this
+older model router gets kicked out from wifi when this happens. By scanning the access points and retrieving correct 
+channel info the connection can be reestablished in an automatic fashion.
+
+The windows executable can be created by running command "python setup.py py2exe".
+Parameters are stored in config.json file and that must be in the same directory as the executable.
--- main.py
+++ main.py
@@ -5,14 +5,9 @@ from requests.exceptions import ConnectionError
 import platform  # For getting the operating system name
 import subprocess  # For executing a shell command
 
-
-# config = {"wan_IP": "192.168.0.1",
-#           "router_IP": "192.168.1.1",
-#           "http_username": "admin",
-#           "http_password": "admin",
-#           "ssid": "SerEla",
-#           "wifi_password": "novamulher01"}
-
+# formato do config.json
+# {"wan_IP": "xxx.xxx.xxx.xxx", "router_IP": "zzz.zzz.zzz.zzz", "http_username": "admin", "http_password": "admin",
+# "ssid": "ssid1", "wifi_password": "changeme"}
 def load_config():
     f = open('config.json')
     return json.load(f)
@@ -40,32 +35,30 @@ def query_aps():
             # http a resposta acaba causando com uma exceção, felizmente a resposta que o roteador forneceu é adicionada
             # ao erro da excessão tornando possível capturar a exception para extrair a resposta
             r = str(e)
-    # r = "('Connection aborted.', BadStatusLine('PERGUNTAPROCACHORRO,e0:1c:fc:a0:27:06,1,AES  ,5    ;
-    # SUAVEDEPIL 2.4,ac:f9:70:42:c9:80,1,AES  ,34   ;Jujubeachwear,98:da:c4:eb:19:d2,2,AES  ,20   ;
-    # Madame Gi_2G,14:6b:9a:16:43:82,4,AES  ,15   ;Tenda_FAD340,e8:65:d4:fa:d3:40,4,NONE ,15   ;
-    # SerEla,e4:c3:2a:66:17:c8,10,AES  ,100  ;Conceito Studio,98:da:c4:89:c4:74,11,AES  ,15   ;
-    # AkiSushiBAR_WIFI,54:e6:fc:a9:46:ea,13,AES  ,24   ;LIVE TIM_0176_2G,8c:dc:02:16:fb:66,13,AES  ,0    \r\n'))"
+    # r = "('Connection aborted.', BadStatusLine('SSID1,ab:bc:cd:de:ef:ff,1,AES  ,100  ;
+    # SSID2,00:11:22:33:44:55,11,TKIP ,34   ;SSID3,ff:fe:ed:dc:cb:ba,13,NONE ,0    \r\n'))"
     # remove a porção inicial ate o segundo parentesis como tambem os parentesis e caracteres de escape ao final da
     # string, fazendo uma conversão para lista separando no ";"
     lista_crua = ",".join(r[1:-1].split(",")[1:])[16:-6].split(";")
     # print(lista_crua)
-    # quebra a lista em uma nova lista de listas, com todos os caracteres de espaco removidos
+    # quebra a lista em uma nova lista de listas, com todos os caracteres de espaco ao fim de cada item removidos
     saida = list(map(lambda x: list(map(lambda y: y.strip(), x.split(","))), lista_crua))
     return saida
 
 
 def get_wifi_data():
+    # faz a pesquisa dos aps proximos e filtra os resultados para apenas mostrar do ssid fornecido
     dados = query_aps()
     wifi_data = list(filter(lambda x: x[0] == config["ssid"], dados))[0]
     return wifi_data
 
 
 def upload_wifi_data(wifi_data):
-    # ['SerEla', 'e4:c3:2a:66:17:c8', '10', 'AES', '100']
-    # wlMode=1&sta_ssid=SerEla&sta_mac=e4%3Ac3%3A2a%3A66%3A17%3Ac8&sta_channel=10&sta_security_mode=3&wep_mode=0&
-    # wep_default_key=1&WEPSelect=1&wep_key_1=&wep_key_2=&wep_key_3=&wep_key_4=&cipher=1&passphrase=novamulher01&
+    # wifi_data = ['ssid1', 'ab:bc:cd:de:ef:ff', '1', 'AES', '100']
+    # postdata - wlMode=1&sta_ssid=ssid1&sta_mac=ab%3Abc%3Acd%3Ade%3Aef%3Aff&sta_channel=11&sta_security_mode=3&wep_mode=0&
+    # wep_default_key=1&WEPSelect=1&wep_key_1=&wep_key_2=&wep_key_3=&wep_key_4=&cipher=1&passphrase=changeme&
     # wlsSlt=radiobutton
-    # 0:close 1;wep 2:wpa 3:wpa2 ou AES
+    # codigo de encriptacao e numeros - 0:close 1;wep 2:wpa 3:wpa2
     sec_mode = {'NONE': 0,
                 'WEP': 1,
                 'TKIP': 2,
@@ -87,14 +80,17 @@ def upload_wifi_data(wifi_data):
             'passphrase': config["wifi_password"],
             'wlsSlt': 'radiobutton'
             }
-    # print(data)
-    print("As configurações foram atualizadas!")
-    r = requests.post(url=urlpost, data=data, auth=(config["http_username"], config["http_password"]))
-    # print(r.text)
+    try:
+        r = requests.post(url=urlpost, data=data, auth=(config["http_username"], config["http_password"]))
+        print("As configurações foram atualizadas!")
+    except Exception as e:
+        print("Error! Não foi possível salvar as configurações... " + str(e))
 
 
 def router_reboot():
-    print("Reiniciando roteador com novas configurações...")
+    # reinicia roteador. Como o roteador reinicia antes de responder ao GET request ocorre uma ConnectionError exception
+    # antes do codigo poder prosseguir...
+    print("Reiniciando roteador com novas configurações... Isso pode demorar alguns instantes...")
     try:
         requests.get(urlreboot, auth=(config["http_username"], config["http_password"]))
     except ConnectionError as e:
@@ -102,6 +98,7 @@ def router_reboot():
 
 
 def ping(host):
+    # reference - https://stackoverflow.com/questions/2953462/pinging-servers-in-python
     """
     Returns True if host (str) responds to a ping request.
     Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
@@ -116,7 +113,9 @@ def ping(host):
     return subprocess.call(command) == 0
 
 
-def reconnect_wifi_reboot():
+def test_reconnect_reboot():
+    # testa conectividade com a rede ip do access point e se falhar reenvia os dados corretos para apos o
+    # reboot o roteador se conectar na rede wifi corretamente
     if ping(config["router_IP"]):
         print("Procurando " + config["ssid"] + " ...")
         dado = get_wifi_data()
@@ -136,11 +135,13 @@ def reconnect_wifi_reboot():
 
 
 def test_wifi_is_conected():
+    # testa se a rede ip da rede wifi esta acessivel
     return ping(config["wan_IP"])
 
 
 if __name__ == "__main__":
+    # testa a conectividade com a rede wifi, se nao houver conectividade tenta reconectar
     if not test_wifi_is_conected():
-        reconnect_wifi_reboot()
+        test_reconnect_reboot()
     else:
         print("Wifi está conectada! :>")
